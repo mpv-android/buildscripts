73f6c5d5890e7c491896c7edd78771ee45895cfb
Ilya Zhuravlev <whatever@xyz.is>
2016-02-08 23:23:39 +0300

    update build-mpv.sh and fix compiler hack

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -8,14 +8,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH="$DIR/../bin:$PATH"
 
 FFPATH=`pwd`/../ffmpeg/_build/out
+ASPATH=`pwd`/../libass/_build/out
 ALPATH=`pwd`/../openal-soft-android/_build/out
+# required by libass:
+FTPATH=`pwd`/../freetype2/_build/out
+FBPATH=`pwd`/../fribidi/_build/out
 
-./bootstrap.py
+[ -f waf ] || ./bootstrap.py
 
 CC=arm-linux-androideabi-gcc LDFLAGS="-llog -lOpenSLES" \
-PKG_CONFIG_LIBDIR="$FFPATH/lib/pkgconfig:$ALPATH/lib/pkgconfig" \
+PKG_CONFIG_LIBDIR="$FFPATH/lib/pkgconfig:$ASPATH/lib/pkgconfig:$ALPATH/lib/pkgconfig:$FTPATH/lib/pkgconfig:$FBPATH/lib/pkgconfig" \
 ./waf configure \
-	--enable-android --disable-iconv --disable-libass --enable-openal \
+	--enable-android --disable-iconv --enable-openal \
 	--enable-libmpv-shared \
 	--prefix=/ --disable-debug-build --disable-manpage-build \
 	-o "`pwd`/_build"
