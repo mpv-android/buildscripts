0ff02b188ea32b4b445d9eef55941e96fc28e424
sfan5 <sfan5@live.de>
2016-09-11 15:33:30 +0200

    Multi arch support

--- scripts/fribidi.sh
+++ scripts/fribidi.sh
@@ -5,7 +5,7 @@
 if [ "$1" == "build" ]; then
 	true
 elif [ "$1" == "clean" ]; then
-	rm -rf _build
+	rm -rf _build$dir_suffix
 	exit 0
 else
 	exit 255
@@ -13,14 +13,14 @@ fi
 
 # You'll need to use the non-git version of fribidi as the git version is missing some files
 
-mkdir -p _build
-cd _build
+mkdir -p _build$dir_suffix
+cd _build$dir_suffix
 
 PKG_CONFIG=/bin/false \
 ../configure \
-	--host=arm-linux-androideabi \
+	--host=$ndk_triple \
 	--enable-static --disable-shared \
-	--prefix="`pwd`/../../../prefix"
+	--prefix="`pwd`/../../../prefix$dir_suffix"
 
 make -j6
 make install
--- scripts/lua.sh
+++ scripts/lua.sh
@@ -11,23 +11,25 @@ else
 	exit 255
 fi
 
+# FIXME: multiarch doesn't work correctly yet (built in src dir)
+
 # LUA_T= and LUAC_T= disable building lua & luac
 # -Dgetlocaledecpoint()=('.') fixes bionic missing decimal_point in localeconv
-make CC="arm-linux-androideabi-gcc -Dgetlocaledecpoint\(\)=\(\'.\'\)" \
-	AR="arm-linux-androideabi-ar r" \
-	RANLIB="arm-linux-androideabi-ranlib" \
+make CC="$ndk_triple-gcc -Dgetlocaledecpoint\(\)=\(\'.\'\)" \
+	AR="$ndk_triple-ar r" \
+	RANLIB="$ndk_triple-ranlib" \
 	PLAT=linux LUA_T= LUAC_T= -j6
 
 INSTALL=install
 [ "$os" == "macosx" ] && INSTALL=ginstall
 
 # TO_BIN=/dev/null disables installing lua & luac
-make INSTALL="$INSTALL" INSTALL_TOP=`pwd`/../../prefix TO_BIN=/dev/null install
+make INSTALL="$INSTALL" INSTALL_TOP=`pwd`/../../prefix$dir_suffix TO_BIN=/dev/null install
 
 # make pc only generates a partial pkg-config file because ????
-mkdir -p ../../prefix/lib/pkgconfig
-make INSTALL_TOP=`pwd`/../../prefix pc > ../../prefix/lib/pkgconfig/lua.pc
-cat >>../../prefix/lib/pkgconfig/lua.pc <<'EOF'
+mkdir -p ../../prefix$dir_suffix/lib/pkgconfig
+make INSTALL_TOP=`pwd`/../../prefix pc > ../../prefix$dir_suffix/lib/pkgconfig/lua.pc
+cat >>../../prefix$dir_suffix/lib/pkgconfig/lua.pc <<'EOF'
 Name: Lua
 Description:
 Version: ${version}
