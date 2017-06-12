mpv-android/buildscripts

commit cec0d466eb3b5e74d7f3383262a3fa30712e1b9c

Add missing cpu/fpu flags
While this might only be a speedup for arm, it will fix compiling ffmpeg for aarch64.

scripts/ffmpeg.sh
@@ -17,12 +17,15 @@ cd _build$dir_suffix
 cpu=armv7-a
 [[ "$ndk_triple" == "aarch64"* ]] && cpu=armv8-a
 
+cpuflags="-mfpu=neon"
+[[ "$ndk_triple" == "arm"* ]] && cpuflags="$cpuflags -mcpu=cortex-a8"
+
 prefix="`pwd`/../../../prefix$dir_suffix"
 PKG_CONFIG_LIBDIR="$prefix/lib/pkgconfig" \
 ../configure \
 	--target-os=android --enable-cross-compile --cross-prefix=$ndk_triple- --cc=$CC \
 	--arch=${ndk_triple%%-*} --cpu=$cpu --enable-{jni,mediacodec,gmp,gnutls} \
-	--extra-cflags="-I$prefix/include" --extra-ldflags="-L$prefix/lib" \
+	--extra-cflags="-I$prefix/include $cpuflags" --extra-ldflags="-L$prefix/lib" \
 	--disable-static --enable-shared --enable-version3 \
 	--prefix="$prefix" --pkg-config=pkg-config --disable-{debug,doc}
 

commit cfe12f2acdd6477b6568d78124a0f9f0d4d941f1

Fix arm64 gcc build of ffmpeg
Unlike clang, gcc does not accept an erroneous -mfpu=neon for AArch64.
Broken since cec0d466eb3b5e74d7f3383262a3fa30712e1b9c

scripts/ffmpeg.sh
@@ -17,8 +17,8 @@ cd _build$dir_suffix
 cpu=armv7-a
 [[ "$ndk_triple" == "aarch64"* ]] && cpu=armv8-a
 
-cpuflags="-mfpu=neon"
-[[ "$ndk_triple" == "arm"* ]] && cpuflags="$cpuflags -mcpu=cortex-a8"
+cpuflags="-ftree-vectorize"
+[[ "$ndk_triple" == "arm"* ]] && cpuflags="$cpuflags -mfpu=neon -mcpu=cortex-a8"
 
 prefix="`pwd`/../../../prefix$dir_suffix"
 PKG_CONFIG_LIBDIR="$prefix/lib/pkgconfig" \